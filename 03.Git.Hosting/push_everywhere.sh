db13ec6b39b4635f28684c390b0e30055c969928
Egor Voronov <eggggor@mail.ru>
2021-06-16 10:27:56 +0300

    Editing .md in 03.Git.Hosting

--- 03.Git.Hosting/push_everywhere.sh
+++ 03.Git.Hosting/push_everywhere.sh
@@ -7,7 +7,7 @@ git add --all
 echo "Specify a commit message:"
 read commit
 git commit -m "$commit"
-git push --all bitbucket
-git push --all gitlab
-git push --all github
+git push --all bitbucket -f
+git push --all gitlab -f
+git push --all github -f
 echo "All files and branches is pushed!"
